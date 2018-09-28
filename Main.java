43184a2b2b593cc2c7c6509dcdbe020101ee9a5d
kaufmannalan <42677923+kaufmannalan@users.noreply.github.com>
2018-09-27 22:06:09 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,7 +22,7 @@ public class Main {
         double damage10 = Int * 5 * (rand.nextDouble()+0.5);
       double Finaldamage=damage1+damage2+damage2+damage3+damage4+damage5+damage6+damage7+damage8+damage9+damage10;
         System.out.println();
-        System.out.println("Final damage output:  "+Finaldamage+"");
+        System.out.println("Final damage output:  "+Finaldamage+" at 20 Int");
 
 
 
